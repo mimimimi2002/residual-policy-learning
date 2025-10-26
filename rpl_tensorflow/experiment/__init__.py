9a1c6bb0404e336f7204e0573058f3c81a445ab8
mimimimi2002 <mikimiki2002.ma@gmail.com>
2025-10-26 21:23:28 +0900

    can run train_residual_base.py

---
+++ rpl_tensorflow/experiment/__init__.py
@@ -0,0 +1,18 @@
+import sys
+
+# :'(
+from rpl_tensorflow.experiment.configs import config_copy
+sys.path.append("/home/miki/residual-policy-learning/rpl_tensorflow")
+import experiment.models as models
+from experiment import ddpg_controller
+# from . import ddpg_controller_residual_base
+sys.modules['config_copy'] = config_copy
+sys.modules['models'] = models
+sys.modules['ddpg_controller'] = ddpg_controller
+# sys.modules['ddpg_controller_residual_base'] = ddpg_controller_residual_base
+
+import pickle
+
+def load_her_policy(filepath):
+    with open(filepath, 'rb') as f:
+        return pickle.load(f)
